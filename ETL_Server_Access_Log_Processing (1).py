3a87e4137b3be6ee781082a8dc6e8557feb2c1a8
Swarnamayee Polaki <68282061+P-Swarnamayee@users.noreply.github.com>
2024-04-19 00:17:44 -0400

    Add files via upload

---
+++ ETL_Server_Access_Log_Processing (1).py
@@ -0,0 +1,49 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'John Adams',
+    'start_date': days_ago(0),
+    'email': ['john.a@somemail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    dag_id='etl-server-access-dag',
+    default_args=default_args,
+    description='DAG for ETL Server Access Log Processing using Bash',
+    schedule_interval=timedelta(days=1),
+)
+
+download = BashOperator(
+    task_id='download',
+    bash_command='wget "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Apache%20Airflow/Build%20a%20DAG%20using%20Airflow/web-server-access-log.txt"',
+    dag=dag,
+)
+
+extract = BashOperator(
+    task_id='extract',
+    bash_command='cut -f1,4 -d"#" web-server-access-log.txt > /home/project/airflow/dags/extracted.txt',
+    dag=dag,
+)
+
+transform = BashOperator(
+    task_id='transform',
+    bash_command='tr "[a-z]" "[A-Z]" < /home/project/airflow/dags/extracted.txt > /home/project/airflow/dags/capitalized.txt',
+    dag=dag,
+)
+
+load = BashOperator(
+    task_id='load',
+    bash_command='zip log.zip capitalized.txt',
+    dag=dag,
+)
+
+
+download >> extract >> transform >> load
+
